3fb42281bb1aaf5074f1d138f9113ff7b7afaab4
sina <sina.luecke@web.de>
2023-01-15 13:27:20 +0100

    idea to add a bool variable

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 int main(void) {
 
     // Hello World print
     printf("I like Pizza!\n");
     printf("Even though Pasta is better.\n");
+    printf("\n");
 
     // Variable Tests
     int age=19;
@@ -11,10 +14,16 @@ int main(void) {
 
     float gpa=2.05;
     char grade='B';
-    char name[] = "Sina";
+    char name[] = "Sina";                                            //array of chars for a string
 
-    printf("%s, you are %i Years old.\n", name, age);
-    printf("In march, you will be %i years old.\n", ageNextYear);
+    printf("%s, you are %i Years old.\n", name, age);                //%s for string (char array) + %i or %d for int 
+    printf("In march, you will be %i years old.\n", ageNextYear);  
+    printf("Your average grade is %f.\n", gpa);                      //%f for float
+    printf("Your last grade was a %c.\n", grade);                    //%c for char
     
+
+    bool e = true; 
+    printf("Did you study for your test? Answer: %d\n", e);          //%d for boolean
+
     return 0;
 }
