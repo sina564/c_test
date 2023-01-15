6ebd0754ecae4b1e53135305e6426cba7acaf521
sina <sina.luecke@web.de>
2023-01-15 13:10:07 +0100

    added code

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 int main(void) {
 
+    // Hello World print
     printf("I like Pizza!\n");
     printf("Even though Pasta is better.\n");
+
+    // Variable Tests
+    int age=19;
+    int ageNextYear=age+1;
+
+    float gpa=2.05;
+    char grade='B';
+    char name[] = "Sina";
+
+    printf("%s, you are %i Years old.\n", name, age);
+    printf("In march, you will be %i years old.\n", ageNextYear);
     
     return 0;
 }
